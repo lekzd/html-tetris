895c9dcb66ceb47b43cbf3dd44ad3cd2c873930e
a.korotaev2 <a.korotaev2@tinkoff.ru>
2019-06-05 01:54:52 +0300

    randomized attribute values

--- src/nodes/InputNode.ts
+++ src/nodes/InputNode.ts
@@ -1,7 +1,35 @@
 import {Node} from "./Node";
+import {randomChoice} from '../utils/random';
+
+const randomTypes = [
+  'text',
+  'password',
+  'submit',
+  'image',
+  'date',
+  'datetime',
+  'range',
+  'checkbox',
+  'radio',
+];
+
+const randomNames = [
+  'name',
+  'password',
+];
 
 export class InputNode extends Node {
 
   childless = true;
+
+  onAddAttribute(name: string) {
+    if (name === 'type') {
+      this.attributes.set(name, randomChoice(randomTypes));
+    }
+
+    if (name === 'name') {
+      this.attributes.set(name, randomChoice(randomNames));
+    }
+  }
 
 }
--- src/nodes/Node.ts
+++ src/nodes/Node.ts
@@ -1,4 +1,42 @@
 import {Dom} from "../entities/Dom";
+import {randomChoice} from '../utils/random';
+
+const randomClasses = [
+  'b__element--modificator',
+  'item',
+  'block',
+  'button',
+  'hidden',
+  'header',
+  'footer',
+  'pt20 pb20 pl10 pr10',
+];
+
+const randomIds = [
+  'first',
+  'second',
+  'third',
+  'header',
+];
+
+const randomSizes = [
+  '100%',
+  '300px',
+  '50%',
+  '100px',
+];
+
+const randomStyles = [
+  'position: relative; z-index: 1',
+  'visibility: hidden',
+  'width: auto',
+  'height: auto',
+];
+
+const randomJS = [
+  'alert("hi!")',
+  'history.back()',
+];
 
 export class Node {
 
@@ -20,6 +58,31 @@ export class Node {
     }
 
     this.attributes.set(name, '');
+
+    if (name === 'class') {
+      this.attributes.set(name, randomChoice(randomClasses));
+    }
+
+    if (name === 'id') {
+      this.attributes.set(name, randomChoice(randomIds));
+    }
+
+    if (name === 'width') {
+      this.attributes.set(name, randomChoice(randomSizes));
+    }
+
+    if (name === 'height') {
+      this.attributes.set(name, randomChoice(randomSizes));
+    }
+
+    if (name === 'style') {
+      this.attributes.set(name, randomChoice(randomStyles));
+    }
+
+    if (name === 'onclick') {
+      this.attributes.set(name, randomChoice(randomJS));
+    }
+
     this.onAddAttribute(name);
   }
 
